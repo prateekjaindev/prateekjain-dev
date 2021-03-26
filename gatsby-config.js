a9328fd29d47c540c3e52aecb750662e2e5ebe7b
Prateek Jain <prateekjain0495@gmail.com>
2021-03-26 21:15:13 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 266915756,
+        trackingId: "266915756",
       },
     },
     {
