2f5c1c9abf6ef7c591db939c5383e95a83b5faf0
Prateek Jain <prateekjain0495@gmail.com>
2021-03-26 21:09:27 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,11 @@ module.exports = {
       // See the theme's README for all available options
       options: {},
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+   # {
+   #   resolve: `gatsby-plugin-google-analytics`,
+   #   options: {
+   #     trackingId: process.env.GOOGLE_ANALYTICS_ID,
+   #   },
     },
     {
       resolve: `gatsby-plugin-manifest`,
